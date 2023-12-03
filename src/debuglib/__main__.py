f8bfaec2f8150180c7d41243e449810ab53957de
PlayerG9 <g9yout@gmail.com>
2023-12-03 16:59:05 +0100

    made CLI usable and added `debuglib listen` as the first command

--- src/debuglib/__main__.py
+++ src/debuglib/__main__.py
@@ -5,16 +5,37 @@ r"""
 import argparse as ap
 import sys
 from . import __description__, __version__
+from .defaults import DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT
 
 
-parser = ap.ArgumentParser(prog='debuglib', description=__description__)
+parser = ap.ArgumentParser(
+    prog='debuglib',
+    description=__description__,
+    formatter_class=ap.ArgumentDefaultsHelpFormatter
+)
 parser.add_argument('-v', '--version', action='version', version=__version__)
+parser.set_defaults(fn=parser.print_help)
+subparser = parser.add_subparsers()
+
+
+def cmd_listen(host: str = None, port: int = None):
+    from ._cli import CLIListener
+    listener = CLIListener((host, port))
+    listener.run()
+
+
+listen_parser = subparser.add_parser(name="listen", formatter_class=ap.ArgumentDefaultsHelpFormatter)
+listen_parser.set_defaults(fn=cmd_listen)
+listen_parser.add_argument('--host', type=str, default=DEFAULT_SERVER_HOST,
+                           help='host to bind to')
+listen_parser.add_argument('--port', type=int, default=DEFAULT_SERVER_PORT,
+                           help="port to listen on")
 
 
 def main():
-    parser.parse_args()
-    print("Currently under development")
-    return 0
+    args = vars(parser.parse_args())
+    fn = args.pop('fn')
+    return fn(**args) or 0
 
 
 if __name__ == '__main__':
