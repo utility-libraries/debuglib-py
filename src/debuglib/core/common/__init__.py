86bf71d0500ad55deb0e2d391d07c874995643ee
PlayerG9 <g9yout@gmail.com>
2023-12-03 23:40:54 +0100

    added ?soon? to be used get_prog() in core.common

--- src/debuglib/core/common/__init__.py
+++ src/debuglib/core/common/__init__.py
@@ -3,3 +3,4 @@ r"""
 
 """
 from .extract_server_info import extract_server_info
+from .prog import get_prog
---
+++ src/debuglib/core/common/prog.py
@@ -0,0 +1,20 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import functools
+
+
+@functools.cache
+def get_prog() -> str:
+    import sys, os  # noqa: E401
+    main = sys.modules.get('__main__')
+    if not main:
+        return "<unknown>"
+    file = getattr(main, '__file__', '<unknown>')
+    if file.startswith('<') and file.endswith('>'):
+        return file
+    filename, _ = os.path.splitext(os.path.basename(file))
+    if filename in {'__main__', '__init__'}:
+        filename = os.path.basename(os.path.dirname(file))
+    return filename
