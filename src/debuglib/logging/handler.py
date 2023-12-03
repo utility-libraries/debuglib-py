aa7e2a625a03c0d1f4412e876fc5dfa87dd1ef11
PlayerG9 <g9yout@gmail.com>
2023-12-03 23:41:15 +0100

    implemented very basic logging handler

--- src/debuglib/logging/handler.py
+++ src/debuglib/logging/handler.py
@@ -2,12 +2,24 @@
 r"""
 logging-handler to send messages to the debug-server
 """
-import logging.handlers
+import logging
+from ..typing import ServerInfoRaw
+from ..core import DebugClient
 
 
 class BlockingDebugHandler(logging.Handler):
     # creates connection to server
-    pass
+    def __init__(self, server_info: ServerInfoRaw = None):
+        super().__init__()
+        self._client = DebugClient(server_info=server_info)
+
+    def emit(self, record: logging.LogRecord):
+        self._client.send(
+            message=f"{record.levelname} | {record.getMessage()}",
+            # body=...,
+            exception=record.exc_info[1] if record.exc_info else None,
+            timestamp=record.created,
+        )
 
 
 class NonBlockingDebugHandler(logging.Handler):
