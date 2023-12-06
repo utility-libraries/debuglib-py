0373ab838c75cea338bd419bafa8a48c8446ac6a
PlayerG9 <g9yout@gmail.com>
2023-12-07 00:25:20 +0100

    added error handling for client

--- src/debuglib/core/client/__init__.py
+++ src/debuglib/core/client/__init__.py
@@ -10,8 +10,9 @@ Normal Message:
 - j: json encoded body
 {m|j}{len:2}{body}
 """
-import socket
+import sys
 import time
+import socket
 import typing as t
 # import uuid
 # noinspection PyPep8Naming
@@ -26,23 +27,33 @@ except ModuleNotFoundError:
     from json import dumps as dump_body
     body_format_identifier = b'j'
 try:
+    from better_exceptions import format_exception
     from better_exceptions.formatter import ExceptionFormatter
 
     def format_traceback(tb):
         formatter = ExceptionFormatter()
         return [formatter.format_traceback(tb)[0]]
 except ModuleNotFoundError:
-    from traceback import format_tb as format_traceback
+    from traceback import format_exception, format_tb as format_traceback
+
+
+T_CB_ON_ERROR = t.Callable[[Exception], None]
 
 
 class DebugClient:
+    _server_info: ServerInfo
+    _timeout: float
     _conn: t.Optional[socket.socket]
+    _on_error: t.List[T_CB_ON_ERROR]
+    _print_on_error: bool
+    _last_attempt: float  # todo limit connection attempts
 
     def __init__(self, *, server_info: ServerInfoRaw = None, timeout: float = None):
-        self._server_info: ServerInfo = extract_server_info(server_info)
-        self._timeout: float = timeout
-        self._conn = None
-        self._queue = None
+        self._server_info = extract_server_info(server_info)
+        self._timeout = timeout
+        self._conn = self.create_connection()
+        self._on_error = []
+        self._print_on_error = False
 
     def __del__(self):
         self.close()
@@ -51,6 +62,23 @@ class DebugClient:
         if self._conn is not None:
             self._conn.close()
             self._conn = None
+
+    def on_error(self, callback: T_CB_ON_ERROR):
+        self._on_error.append(callback)
+        return callback
+
+    def print_on_error(self, enable: bool = True):
+        self._print_on_error = enable
+
+    def _handle_error(self, error: Exception):
+        if self._print_on_error:
+            sys.stderr.write('\n'.join(format_exception(type(error), error, error.__traceback__)))
+
+        for callback in self._on_error:
+            try:
+                callback(error)
+            except Exception as cb_err:
+                sys.stderr.write('\n'.join(format_exception(type(cb_err), cb_err, cb_err.__traceback__)))
 
     def send(self, message: str,
              *, body: t.Optional[str] = None, exception: t.Optional[BaseException] = None, timestamp: float = None):
@@ -101,8 +129,10 @@ class DebugClient:
             handshake = b'DEBUGLIB\0' + version_bytes + b'\0'
             sock.sendall(handshake)
             if sock.recv(1) != b'1':
+                self._handle_error(PermissionError("connection was refused"))
                 sock.close()
                 return None
             return sock
-        except (socket.error, socket.timeout, TimeoutError):
+        except (socket.error, socket.timeout, TimeoutError) as error:
+            self._handle_error(error)
             return None
