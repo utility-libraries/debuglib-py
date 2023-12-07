96ba50cb6c16d2772957b8a6e5bad12d1349f6b7
PlayerG9 <g9yout@gmail.com>
2023-12-07 14:04:17 +0100

    added client arguments to decorator.py

--- src/debuglib/decorator/decorator.py
+++ src/debuglib/decorator/decorator.py
@@ -11,8 +11,11 @@ from ._util import function_repr, format_delta_ns
 
 
 class Decorator:
-    def __init__(self, server: ServerInfoRaw = DEFAULT_VALUE):
-        self._client = DebugClient(server_info=server)
+    def __init__(self, server_info: ServerInfoRaw = DEFAULT_VALUE,
+                 *, timeout: float = DEFAULT_VALUE, connection_attempt_delta: float = DEFAULT_VALUE):
+        self._client = DebugClient(
+            server_info=server_info, timeout=timeout, connection_attempt_delta=connection_attempt_delta
+        )
 
     def __del__(self):
         self._client.close()
@@ -20,11 +23,11 @@ class Decorator:
     def reset_connection(self):
         self._client.close()
 
-    def monitor(self, precision: int = 2):
+    def monitor(self, time_precision: int = 2):
         r"""
         monitor a sync/async function and sending the information (arguments, return-value, time) to the debug-server
 
-        :param precision: time-precision
+        :param time_precision: 1=>1s | 2=>1s+1ms | 3=>1s+1ms+1μs | ...
         :return: decorator
         """
 
@@ -37,14 +40,14 @@ class Decorator:
                     try:
                         value = await fn(*args, **kwargs)
                         total_time_ns = time.perf_counter_ns() - start_time
-                        time_repr = format_delta_ns(total_time_ns, precision=precision)
+                        time_repr = format_delta_ns(total_time_ns, precision=time_precision)
                         self._client.send(
                             message=f"{fn_repr} returned {value!r} after {time_repr}",
                         )
                         return value
                     except BaseException as error:
                         total_time_ns = time.perf_counter_ns() - start_time
-                        time_repr = format_delta_ns(total_time_ns, precision=precision)
+                        time_repr = format_delta_ns(total_time_ns, precision=time_precision)
                         self._client.send(
                             message=f"{fn_repr} failed with {type(error).__name__} after {time_repr}",
                             exception=error,
@@ -58,14 +61,14 @@ class Decorator:
                     try:
                         value = fn(*args, **kwargs)
                         total_time_ns = time.perf_counter_ns() - start_time
-                        time_repr = format_delta_ns(total_time_ns, precision=precision)
+                        time_repr = format_delta_ns(total_time_ns, precision=time_precision)
                         self._client.send(
                             message=f"{fn_repr} returned {value!r} after {time_repr}",
                         )
                         return value
                     except BaseException as error:
                         total_time_ns = time.perf_counter_ns() - start_time
-                        time_repr = format_delta_ns(total_time_ns, precision=precision)
+                        time_repr = format_delta_ns(total_time_ns, precision=time_precision)
                         self._client.send(
                             message=f"{fn_repr} failed with {type(error).__name__} after {time_repr}",
                             exception=error,
@@ -75,14 +78,22 @@ class Decorator:
         return decorator
 
 
-def monitor(*, server: ServerInfoRaw = None, precision: int = 2):
+def monitor(*, server_info: ServerInfoRaw = DEFAULT_VALUE,
+            timeout: float = DEFAULT_VALUE, connection_attempt_delta: float = DEFAULT_VALUE,
+            time_precision: int = 2):
     r"""
     monitor a sync/async function and sending the information (arguments, return-value, time) to the debug-server
 
     recommended to use the Decorator-class for better performance through one connection to the server
 
-    :param server: server information
-    :param precision: time-precision
+    :param server_info: server information
+    :param timeout: socket timeout
+    :param connection_attempt_delta: delta between connection attempts
+    :param time_precision: time-precision
     :return: decorator
     """
-    return Decorator(server=server).monitor(precision=precision)
+    return Decorator(
+        server_info=server_info, timeout=timeout, connection_attempt_delta=connection_attempt_delta,
+    ).monitor(
+        time_precision=time_precision
+    )
